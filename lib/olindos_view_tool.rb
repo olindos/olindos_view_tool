0996fb276078f5d3f25ba6a9a8f7fa6eadafc37c
olindos <yason4ik87@gmail.com>
2019-02-25 15:02:17 +0200

    olindos initial commit

---
+++ lib/olindos_view_tool.rb
@@ -0,0 +1,7 @@
+require "olindos_view_tool/version"
+require "olindos_view_tool/renderer"
+
+module OlindosViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/olindos_view_tool/version.rb
@@ -0,0 +1,3 @@
+module OlindosViewTool
+  VERSION = "0.1.0"
+end
